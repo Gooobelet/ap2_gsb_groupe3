9976bd4e3793cfa0e65475053d4815cd6f864356
Max <micheletmax07@gmail.com>
2022-11-25 16:41:22 +0100

    <feat>(class) : Création des class Etape et EtapeNormee

--- EtapeNormee.cs
+++ EtapeNormee.cs
@@ -1,10 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Policy;
 using System.Text;
 
 namespace gsb_gesAMM_APP
 {
     class EtapeNormee
     {
+        private string norme;
+        private DateTime dateNorme;
+
+        public EtapeNormee(string norme, DateTime dateNorme)
+        {
+            this.norme = norme;
+            this.dateNorme = dateNorme;
+        }
+
+        public string getNorme()
+        {
+            return this.norme;
+        }
+
+        public DateTime getDateNorme()
+        {
+            return this.dateNorme;
+        }
+
+        public void setNorme(string norme)
+        {
+            this.norme = norme;
+        }
+
+        public void setDateNorme(DateTime dateNorme)
+        {
+            this.dateNorme= dateNorme;
+        }
     }
 }
