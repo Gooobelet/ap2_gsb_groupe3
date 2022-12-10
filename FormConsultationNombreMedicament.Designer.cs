6e89f5cb2a3f4f49fddd283354b77ba40d518fd2
Max <micheletmax07@gmail.com>
2022-12-10 16:53:11 +0100

    <feat>(consultation) : maj listview Consultation Famille

--- FormConsultationNombreMedicament.Designer.cs
+++ FormConsultationNombreMedicament.Designer.cs
@@ -30,29 +30,30 @@ namespace gsb_gesAMM_APP
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormConsultationNombreMedicament));
-            this.listView1 = new System.Windows.Forms.ListView();
+            this.lv_famille = new System.Windows.Forms.ListView();
             this.colH_code_famille = new System.Windows.Forms.ColumnHeader();
             this.colH_designation_famille = new System.Windows.Forms.ColumnHeader();
             this.colH_nb_médicament = new System.Windows.Forms.ColumnHeader();
             this.label1 = new System.Windows.Forms.Label();
-            this.listView2 = new System.Windows.Forms.ListView();
+            this.lv_médicament = new System.Windows.Forms.ListView();
             this.colH_code_famille_médicament = new System.Windows.Forms.ColumnHeader();
             this.colH_nom_médicament = new System.Windows.Forms.ColumnHeader();
             this.SuspendLayout();
             // 
-            // listView1
+            // lv_famille
             // 
-            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.lv_famille.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.colH_code_famille,
             this.colH_designation_famille,
             this.colH_nb_médicament});
-            this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(12, 54);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(535, 419);
-            this.listView1.TabIndex = 0;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.View = System.Windows.Forms.View.Details;
+            this.lv_famille.HideSelection = false;
+            this.lv_famille.Location = new System.Drawing.Point(12, 54);
+            this.lv_famille.Name = "lv_famille";
+            this.lv_famille.Size = new System.Drawing.Size(535, 419);
+            this.lv_famille.TabIndex = 0;
+            this.lv_famille.UseCompatibleStateImageBehavior = false;
+            this.lv_famille.View = System.Windows.Forms.View.Details;
+            this.lv_famille.SelectedIndexChanged += new System.EventHandler(this.lv_famille_SelectedIndexChanged);
             // 
             // colH_code_famille
             // 
@@ -77,18 +78,18 @@ namespace gsb_gesAMM_APP
             this.label1.TabIndex = 1;
             this.label1.Text = "Consultation du nombre de médicament autorisé par famille";
             // 
-            // listView2
+            // lv_médicament
             // 
-            this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.lv_médicament.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.colH_code_famille_médicament,
             this.colH_nom_médicament});
-            this.listView2.HideSelection = false;
-            this.listView2.Location = new System.Drawing.Point(553, 54);
-            this.listView2.Name = "listView2";
-            this.listView2.Size = new System.Drawing.Size(519, 419);
-            this.listView2.TabIndex = 2;
-            this.listView2.UseCompatibleStateImageBehavior = false;
-            this.listView2.View = System.Windows.Forms.View.Details;
+            this.lv_médicament.HideSelection = false;
+            this.lv_médicament.Location = new System.Drawing.Point(553, 54);
+            this.lv_médicament.Name = "lv_médicament";
+            this.lv_médicament.Size = new System.Drawing.Size(519, 419);
+            this.lv_médicament.TabIndex = 2;
+            this.lv_médicament.UseCompatibleStateImageBehavior = false;
+            this.lv_médicament.View = System.Windows.Forms.View.Details;
             // 
             // colH_code_famille_médicament
             // 
@@ -106,9 +107,9 @@ namespace gsb_gesAMM_APP
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.CornflowerBlue;
             this.ClientSize = new System.Drawing.Size(1084, 621);
-            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.lv_médicament);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.lv_famille);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FormConsultationNombreMedicament";
             this.Text = "Consultation du nombre de médicament";
@@ -120,12 +121,12 @@ namespace gsb_gesAMM_APP
 
         #endregion
 
-        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView lv_famille;
         private System.Windows.Forms.ColumnHeader colH_code_famille;
         private System.Windows.Forms.ColumnHeader colH_designation_famille;
         private System.Windows.Forms.ColumnHeader colH_nb_médicament;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ListView lv_médicament;
         private System.Windows.Forms.ColumnHeader colH_code_famille_médicament;
         private System.Windows.Forms.ColumnHeader colH_nom_médicament;
     }
