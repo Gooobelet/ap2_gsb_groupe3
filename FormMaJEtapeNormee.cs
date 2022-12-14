22dfa10eb9bf9b76175986f5620a610a353e5b38
DNOLLE27 <damien.nolle@stadjutor.com>
2022-12-14 21:47:19 +0100

    <feature>(EtapeNormee) : Contin. interf. MàJ Etape norme + bug rencontre

---
+++ FormMaJEtapeNormee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace gsb_gesAMM_APP
+{
+    public partial class FormMaJEtapeNormee : Form
+    {
+        public FormMaJEtapeNormee()
+        {
+            InitializeComponent();
+        }
+    }
+}
